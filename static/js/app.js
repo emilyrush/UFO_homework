2b6a84097c2d440907718bab19a904e9a831b818
Emily <erushworks@gmail.compu>
2019-03-03 18:15:17 -0500

    Completed baseline requirements for homework. Clears table before filtering, and shows all data before filters are submitted.

--- static/js/app.js
+++ static/js/app.js
@@ -5,11 +5,25 @@ var tbody = d3.select("tbody");
 // Select the submit button
 var submit = d3.select("#filter-btn");
 
+// Show all data
+data.forEach((sightingsList) => {
+    var row = tbody.append("tr");
+    Object.entries(sightingsList).forEach(([key,value]) => {
+        var cell = tbody.append("td");
+        cell.text(value);
+    })
+});
+
 submit.on("click", function() {
   // Prevent the page from refreshing
   d3.event.preventDefault();
+
+  // Clear previous search data
+  tbody.html("");
+
   // Select the input element and get the raw HTML node
   var inputElement = d3.select("#datetime");
+
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
   console.log(inputValue);
@@ -24,16 +38,3 @@ submit.on("click", function() {
     })
 })
 });
-
-
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
-//   var row = tbody.append("tr");
-//   Object.entries(weatherReport).forEach(function([key, value]) {
-//     console.log(key, value);
-//     // Append a cell to the row for each value
-//     // in the weather report object
-//     var cell = tbody.append("td");
-//     cell.text(value);
-//   });
-// });
